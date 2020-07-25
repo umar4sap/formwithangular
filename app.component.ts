67fc0d8083886ff399fce45656cbfee6b79a7979
Mohammad Umar <umarsoft.java@gmail.com>
2020-07-26 00:31:57 +0400

    Update app.component.ts

--- app.component.ts
+++ app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+ 
   
+  constructor() {
+     public progressValue : any = "17%";
+  }
+  
+  trigger(tabType){
+    if(tabType=='about'){
+      this.progressValue="17%";
+    }else if(tabType=='account'){
+      this.progressValue="50%";
+    }else if(tabType=='addres'){
+      this.progressValue="80%";
+    }
+    else if(tabType=='finish'){
+      this.progressValue="100%";
+    }
+  }
 }

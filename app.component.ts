873b01ef7df1af05a3c705f1b434a2f4f346945d
umar4sap <umarsoft.java@gmail.com>
2020-07-27 13:03:56 +0400

    adde changes

--- app.component.ts
+++ app.component.ts
@@ -7,21 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  
-  
-  constructor() {
-     public progressValue : any = "17%";
-  }
-  
-  trigger(tabType){
-    if(tabType=='about'){
-      this.progressValue="17%";
-    }else if(tabType=='account'){
-      this.progressValue="50%";
-    }else if(tabType=='addres'){
-      this.progressValue="80%";
-    }
-    else if(tabType=='finish'){
-      this.progressValue="100%";
-    }
-  }
+
 }
--- app.module.ts
+++ app.module.ts
@@ -5,12 +5,13 @@ import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
+    FormComponent
   ],
   imports: [
     BrowserModule,
